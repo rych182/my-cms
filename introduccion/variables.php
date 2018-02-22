71f3166a7af39e8f6707c28823fd6f5a3f6cb240
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-02-22 03:18:49 -0600

    I added exercises with variables

---
+++ introduccion/variables.php
@@ -0,0 +1,18 @@
+<?php
+
+//Se úeden sumar variables
+	$suma = 5 + 5;
+	$suma2 = 3 + 3;
+	echo $suma + $suma2;
+	echo "<br><br>";
+//Variables boleanas
+
+	$variable = true;
+	echo $variable;
+	echo "<br><br>";
+
+//Variables arreglo
+
+	$colores = array("rojo","azul","verde");
+	echo $colores[0] . $colores[1];
+?>
